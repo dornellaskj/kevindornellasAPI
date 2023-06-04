1f9db95639dfaaa486cb960c8e22b14184892641
Kevin D'Ornellas <dornellaskj@gmail.com>
2023-06-03 19:42:58 -0600

    init api

---
+++ KevinDornellasAPI/Controllers/Multiples.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KevinDornellasAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Multiples : ControllerBase
+    {
+        [HttpGet(Name = "multiples")]
+        public int Get()
+        {
+            List<int> integerList = new List<int>();
+            for(int i = 0; i < 1000; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    integerList.Add(i);
+                }
+            }
+            int returnValue = integerList.Sum();
+            return returnValue;
+        }
+    }
+}
